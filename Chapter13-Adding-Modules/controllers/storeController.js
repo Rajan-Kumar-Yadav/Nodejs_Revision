c61205d89b698ae62f52d1eb4c4c8975541ca350
Rajan kumar <rk2762992@gmail.com>
2025-09-02 19:18:29 +0530

    deleted:    Chapter13-Adding-Modules

--- Chapter13-Adding-Modules/controllers/storeController.js
+++
@@ -1,8 +0,0 @@
-const Home = require("../modules/home")
-
-exports.getHomes = (req,res,next) => {
-  const homes = Home.fetchAll();
-console.log("Registration homepage",homes)
-  res.render('home',{registeredHomes:homes, pageTitle: 'airbnb Home', currentPage: 'Home'})
-  }
-  
