9bc4cac7e3407ad6445855563b0e035bcb877a9b
Rajan kumar <rk2762992@gmail.com>
2025-09-07 09:39:07 +0530

    14.7 Edit-Home: Showing Existing Data and 14.8 Edit-Home: Handing Edit Request

---
+++ Chapter14.7-Edit-Home/controllers/hostController.js
@@ -0,0 +1,51 @@
+const Home = require("../models/home");
+
+exports.getAddHome = (req, res, next) => {
+  res.render("host/edit-home", {
+    pageTitle: "Add Home to airbnb",
+    currentPage: "addHome",
+    editing: false,
+  });
+};
+exports.postAddHome = (req, res, next) => {
+  const { houseName, price, location, rating, photoUrl } = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl);
+  home.save();
+  res.redirect("/host/host-home-list");
+};
+
+exports.getHostHomes = (req, res, next) => {
+  Home.fetchAll((home) => {
+    res.render("host/host-home-list", {
+      registeredHomes: home,
+      pageTitle: "host-home-list",
+      currentPage: "host-homes",
+    });
+  });
+};
+
+exports.getEditHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  const editing = req.query.editing === "true";
+  Home.findById(homeId, (home) => {
+    if (!home) {
+      console.log("home not found for editing");
+      res.redirect("/host/host-home-list");
+    }
+    console.log(homeId, editing, "getEidtHome", home);
+    res.render("host/edit-home", {
+      home: home,
+      pageTitle: "Edit your home",
+      currentPage: "host-homes",
+      editing: editing,
+    });
+  });
+};
+
+exports.postEditHome = (req, res, next) => {
+  const { id, houseName, price, location, rating, photoUrl } = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl);
+  home.id = id;
+  home.save();
+  res.redirect("/host/host-home-list");
+};
---
+++ Chapter14.7-Edit-Home/models/home.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtils");
+const { ref } = require("process");
+const homeDataPath = path.join(rootDir, "data", "homes.json");
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+  }
+
+  save() {
+    Home.fetchAll((registeredHomes) => {
+      if (this.id) {
+        registeredHomes = registeredHomes.map((home) => {
+          if (home.id === this.id) {
+            return this;
+          }
+          return home;
+        });
+      } else {
+        this.id = Math.round(Math.random() * 100).toString();
+        registeredHomes.push(this);
+      }
+      fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) => {
+        console.log("File Writing Concluded", error);
+      });
+    });
+  }
+  static fetchAll(callback) {
+    fs.readFile(homeDataPath, (error, data) => {
+      console.log("errror while reading File", error);
+      if (!error) {
+        callback(JSON.parse(data));
+      } else {
+        callback([]);
+      }
+    });
+  }
+
+  static findById(homeId, callback) {
+    this.fetchAll((homes) => {
+      const homeFound = homes.find((home) => home.id === homeId);
+      callback(homeFound);
+    });
+  }
+};
