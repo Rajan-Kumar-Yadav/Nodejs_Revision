3f2cb8a41c17fbe596e85d18d0bc900008de4e81
Rajan kumar <rk2762992@gmail.com>
2025-09-13 18:56:14 +0530

    fetching homes from homes mysqul homes

---
+++ Chapter15.9-Adding-DB-Models/models/home.js
@@ -0,0 +1,19 @@
+const db = require("../utils/databaseUtil");
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+  }
+
+  save() {}
+  static fetchAll() {
+    return db.execute("SELECT * FROM homes");
+  }
+
+  static findById(homeId, callback) {}
+
+  static deleteById(homeId, callback) {}
+};
