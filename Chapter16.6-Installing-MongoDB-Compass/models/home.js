7449ba97f4681ab00b943d0e2d076d29b38ab9de
Rajan kumar <rk2762992@gmail.com>
2025-09-21 17:13:47 +0530

    Installing MongoDB commpass and Fetching all homes

---
+++ Chapter16.6-Installing-MongoDB-Compass/models/home.js
@@ -0,0 +1,38 @@
+const { getDB } = require("../utils/databaseUtil");
+
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl, description, id) {
+    console.log(
+      "constructor value",
+
+      houseName,
+      price,
+      location,
+      rating,
+      photoUrl,
+      description,
+      id
+    );
+
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+    this.description = description;
+    this.id = id;
+  }
+
+  save() {
+    const db = getDB();
+    return db.collection("homes2").insertOne(this);
+  }
+  static fetchAll() {
+    const db = getDB();
+    return db.collection("homes2").find().toArray();
+  }
+
+  static findById(homeId) {}
+
+  static deleteById(homeId) {}
+};
---
+++ Chapter16.6-Installing-MongoDB-Compass/utils/databaseUtil.js
@@ -0,0 +1,29 @@
+const mongo = require("mongodb");
+
+const MongoClient = mongo.MongoClient;
+
+const MONGO_URL =
+  "mongodb+srv://Rajan:Rajan@rajancoding.hthpzj0.mongodb.net/?retryWrites=true&w=majority&appName=RajanCoding";
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(MONGO_URL)
+    .then((client) => {
+      callback();
+      _db = client.db("airbnb2");
+    })
+    .catch((err) => {
+      console.log("Error while connecting to Mongo", err);
+    });
+};
+
+const getDB = () => {
+  if (!_db) {
+    throw new Error("Mongo not connected");
+  }
+  return _db;
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
