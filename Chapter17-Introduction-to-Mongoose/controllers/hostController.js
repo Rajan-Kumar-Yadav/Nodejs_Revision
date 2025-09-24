ffcc2dd1415c6dd25e794a2a704b9f2af8a308c2
Rajan kumar <rk2762992@gmail.com>
2025-09-24 17:54:35 +0530

    Chapter17-Introduction-to-Mongoose

---
+++ Chapter17-Introduction-to-Mongoose/controllers/hostController.js
@@ -0,0 +1,95 @@
+const Home = require("../models/home");
+
+exports.getAddHome = (req, res, next) => {
+  res.render("host/edit-home", {
+    pageTitle: "Add Home to airbnb",
+    currentPage: "addHome",
+    editing: false,
+  });
+};
+exports.postAddHome = (req, res, next) => {
+  const { houseName, price, location, rating, photoUrl, description } =
+    req.body;
+  const home = new Home({
+    houseName,
+    price,
+    location,
+    rating,
+    photoUrl,
+    description,
+  });
+  home.save().then(() => {
+    console.log("HOme saved successfully");
+  });
+  res.redirect("/host/host-home-list");
+};
+
+exports.getHostHomes = (req, res, next) => {
+  Home.fetchAll().then((registeredHomes) => {
+    res.render("host/host-home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "host-home-list",
+      currentPage: "host-homes",
+    });
+  });
+};
+
+exports.getEditHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  const editing = req.query.editing === "true";
+  Home.findById(homeId).then((home) => {
+    console.log("home afte home 0", home);
+    if (!home) {
+      console.log("home not found for editing");
+      res.redirect("/host/host-home-list");
+    }
+    console.log(homeId, editing, "getEidtHome", home);
+    res.render("host/edit-home", {
+      home: home,
+      pageTitle: "Edit your home",
+      currentPage: "host-homes",
+      editing: editing,
+    });
+  });
+};
+
+exports.postEditHome = (req, res, next) => {
+  const { id, houseName, price, location, rating, photoUrl, description } =
+    req.body;
+  console.log(
+    "Request body",
+    id,
+    houseName,
+    price,
+    location,
+    rating,
+    photoUrl,
+    description
+  );
+  const home = new Home(
+    houseName,
+    price,
+    location,
+    rating,
+    photoUrl,
+    description,
+    id
+  );
+  console.log("home after editing going for saveing", home);
+  home.save().then((result) => {
+    console.log("Home edit sucessesfully", result);
+  });
+  res.redirect("/host/host-home-list");
+};
+
+exports.postDeleteHome = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Home.deleteById(homeId)
+    .then(() => {
+      console.log("home deleted homeId", homeId);
+      res.redirect("/host/host-home-list");
+    })
+    .catch((error) => {
+      console.log("Error while deleting", error);
+    });
+};
---
+++ Chapter17-Introduction-to-Mongoose/models/home.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const homeSchema = mongoose.Schema({
+  houseName: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  photoUrl: String,
+  description: String,
+});
+module.exports = mongoose.model("Home", homeSchema);
